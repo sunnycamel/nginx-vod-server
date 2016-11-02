439261e80ae0b51b98b7da7ca6fc9fe0167d7901
Sun Peng <sunpeng@letv.com>
2016-11-02 17:48:24 +0800

    increase the length of video description

--- script/html/database/migrations/2016_09_03_070428_create_videos_table.php
+++ script/html/database/migrations/2016_09_03_070428_create_videos_table.php
@@ -16,7 +16,7 @@ class CreateVideosTable extends Migration
         Schema::create('videos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('description');
+            $table->string('description', 2048);
             $table->string('video_url');
             $table->string('thumb_picture_url');
             $table->timestamps();
